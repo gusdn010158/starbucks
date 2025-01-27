8eb1114c6a3be00317733cf2b50f24114aaa66b2
gusdn010158 <gusdn010158@gmail.com>
2025-01-27 23:51:54 +0900

    icon react wr

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,3 +1,59 @@
+// import React from "react";
+// import { Link } from "react-router-dom";
+// import TopMenu from "./TopMenu";
+// import BottomMenu from "./BottomMenu";
+// import styled from "styled-components";
+
+// const HeaderWrapper = styled.div`
+//   z-index: 5;
+//   display: flex;
+//   position: fixed;
+//   top: 0px;
+//   width: 100vw;
+//   height: 120px;
+//   background-color: #f4f2eb;
+// `;
+
+// const HeaderIn = styled.div`
+//   display: flex;
+//   width: 100%;
+//   justify-content: space-between;
+//   align-items: center;
+//   margin-right: 380px;
+//   margin-left: 380px;
+// `;
+
+// const HeaderLogo = styled.img`
+//   width: 80px;
+//   object-fit: cover;
+// `;
+
+// const HeaderMain = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   align-content: center;
+// `;
+
+// function Header() {
+//   return (
+//     <HeaderWrapper>
+//       <HeaderIn>
+//         <Link to="/">
+//           <HeaderLogo
+//             src="https://pngimg.com/uploads/starbucks/starbucks_PNG11.png"
+//             alt="Starbucks Logo"
+//           />
+//         </Link>
+//         <HeaderMain>
+//           <TopMenu />
+//           <BottomMenu />
+//         </HeaderMain>
+//       </HeaderIn>
+//     </HeaderWrapper>
+//   );
+// }
+
+// export default Header;
 import React from "react";
 import { Link } from "react-router-dom";
 import TopMenu from "./TopMenu";
@@ -21,17 +77,38 @@ const HeaderIn = styled.div`
   align-items: center;
   margin-right: 380px;
   margin-left: 380px;
+
+  @media (max-width: 1024px) {
+    /* 모바일 중형 모드 */
+    margin-right: 100px;
+    margin-left: 100px;
+  }
+  @media (max-width: 767px) {
+    /* 모바일 중형 모드 */
+    margin-right: 0px;
+    margin-left: 0px;
+  }
 `;
 
 const HeaderLogo = styled.img`
   width: 80px;
   object-fit: cover;
+
+  @media (max-width: 767px) {
+    /* 모바일 중형 모드 */
+    width: 60px;
+  }
 `;
 
 const HeaderMain = styled.div`
   display: flex;
   flex-direction: column;
   align-content: center;
+
+  @media (max-width: 767px) {
+    /* 모바일 중형 모드 */
+    align-items: center;
+  }
 `;
 
 function Header() {
--- src/Header/SearchBar.js
+++ src/Header/SearchBar.js
@@ -3,30 +3,37 @@ import { FaSistrix } from "react-icons/fa";
 import styled from "styled-components";
 
 const Menu = styled.div`
-  right: 0;
   display: flex;
+  align-items: center;
+`;
+
+const InputWrapper = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
 `;
 
 const Input = styled.input`
   width: ${(props) => (props.isFocused ? "180px" : "36px")};
   height: 34px;
-  padding: 4px 10px;
+  padding: 4px 30px 4px 10px; /* 아이콘 크기 고려 */
   border: 1px solid #ccc;
-  box-sizing: border-box;
   border-radius: 5px;
   outline: none;
   background-color: #fff;
   color: #777;
   font-size: 12px;
   transition: width 0.4s;
+  box-sizing: border-box;
 `;
 
 const IconContainer = styled.div`
   position: absolute;
-  margin: auto;
-  top: 17px;
-  right: 384px;
+  right: 10px; /* input 내부 오른쪽 여백 */
   display: flex;
+  align-items: center;
+  justify-content: center;
+  pointer-events: none; /* 아이콘 클릭 불가능 처리 */
 `;
 
 function SearchBar() {
@@ -41,17 +48,19 @@ function SearchBar() {
   }
 
   return (
-    <Menu onClick={handleSearchFocus}>
-      <Input
-        isFocused={isSearchFocused}
-        type="text"
-        onFocus={handleSearchFocus}
-        onBlur={handleSearchBlur}
-        placeholder={isSearchFocused ? "통합검색" : ""}
-      />
-      <IconContainer>
-        <FaSistrix size="26" />
-      </IconContainer>
+    <Menu>
+      <InputWrapper>
+        <Input
+          isFocused={isSearchFocused}
+          type="text"
+          onFocus={handleSearchFocus}
+          onBlur={handleSearchBlur}
+          placeholder={isSearchFocused ? "통합검색" : ""}
+        />
+        <IconContainer>
+          <FaSistrix size="20" />
+        </IconContainer>
+      </InputWrapper>
     </Menu>
   );
 }
